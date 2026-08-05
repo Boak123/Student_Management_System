1cd8144f4fce1f0c47c6edf58448464c3b6fa45b
Boak123 <boluwatifeakorede17@gmail.com>
2026-08-06 00:42:27 +0100

    student management project

---
+++ main.py.py
@@ -0,0 +1,35 @@
+class Person:
+
+    def eat(self):
+        print("I want to eat.")
+
+    def sleep(self):
+        print("I want to sleep.")
+
+class Student(Person):
+
+    def study(self):
+        print("I am Studying")
+
+    def take_exam(self):
+        print("I am taking an exam.")
+
+    def register(self):
+        print("I have registered my courses.")
+
+class Lecturer(Person):
+
+    def teach(self):
+        print("I am teaching.")
+
+class Administrator(Person):
+
+    def admit_student(self):
+        print("Student admitted.")
+
+student1 = Student()
+student1.study()
+student1.take_exam()
+student1.register()
+student1.sleep()
+student1.eat()
