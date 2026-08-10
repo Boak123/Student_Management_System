2971705cec22d544e048929afd42037ccddef0c4
Boak123 <boluwatifeakorede17@gmail.com>
2026-08-10 23:55:57 +0100

    updated

--- main.py.py
+++ main.py.py
@@ -1,12 +1,25 @@
 class Person:
 
+    def __init__(self, name, address, ID):
+        self.name = name
+        self.address = address
+        self.ID = ID
+
     def eat(self):
         print("I want to eat.")
 
     def sleep(self):
         print("I want to sleep.")
 
+    def bath(bath):
+        print("taking my bath")
+
 class Student(Person):
+
+    def __init__(self, studentID, course, timetable):
+        self.studentID = studentID
+        self.course = course
+        self.timetable = timetable
 
     def study(self):
         print("I am Studying")
@@ -17,23 +30,39 @@ class Student(Person):
     def register(self):
         print("I have registered my courses.")
 
-        def under_graduate_student(self):
+class under_graduate_student(Student):
+    def __init__(self, matricNumber, relationship):
+        self.matricNumber = matricNumber
+        self.relationship = relationship
 
-            def attend_lectures(self):
-                print("attending lectures")
+        def attend_lectures(self):
+            print("attending lectures")
 
-            def take_notes(self):
-                print("Taking Notes")
+        def take_notes(self):
+            print("Taking Notes")
 
 class Lecturer(Person):
+
+    def __init__(self, staffID, course_teaching):
+        self.staffID = staffID
+        self.course_teaching = course_teaching
 
     def teach(self):
         print("I am teaching.")
 
 class Administrator(Person):
+    def __init__(self, emergencydata, id):
+        self.emergencydata = emergencydata
+        self.id = id
 
     def admit_student(self):
         print("Student admitted.")
+
+    def assigned_matric_number(self):
+        print("matric assigned")
+
+    def process_school_fees(self):
+        print("processing")
 
 student1 = Student()
 student1.study()
