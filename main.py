3a2def92060d8c319f0c3c83b2b6457ddaa8fd41
Boak123 <boluwatifeakorede17@gmail.com>
2026-09-12 23:05:13 +0100

    updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 from services.student_service import StudentService
+
 from models import (
     Student,
     UnderGraduateStudent,
@@ -7,26 +7,104 @@ from models import (
     Administrator
 )
 
+
 student_service = StudentService()
-student1 = Student("victor", "kwara", 1001, "STOO1", "computer science", "Monday 9AM", 50)
-lecturer1 = Lecturer("Bolu", "kwara", 81111, "tyfu`2", "ICT", 500000)
-administrator1 = Administrator("Akorede", "Kwara", 8909, "ihe8989", "IT", "operator")
-under_graduate1 = UnderGraduateStudent("Bolu", "Kwara", 7043797036, "456gh", "Computer science", 200, 3345, "single", 20) 
+
+
+student1 = Student(
+    "victor",
+    "kwara",
+    "1001",
+    "ST001",
+    "computer science",
+    "100L",
+    50
+)
+
+student2 = Student(
+    "David",
+    "Lagos",
+    "1002",
+    "ST002",
+    "computer science",
+    "100L",
+    80
+)
+
+lecturer1 = Lecturer(
+    "Bolu",
+    "Kwara",
+    "81111",
+    "STA001",
+    "ICT",
+    500000
+)
+
+administrator1 = Administrator(
+    "Akorede",
+    "Kwara",
+    "8909",
+    "ADM001",
+    "IT",
+    "operator"
+)
+
+under_graduate1 = UnderGraduateStudent(
+    "Bolu",
+    "Kwara",
+    "7043797036",
+    "ST003",
+    "Computer science",
+    "200L",
+    "3345",
+    "single",
+    20
+)
+
+
+# Register students
 student_service.register_student(student1)
-student_service.display_student(student1)
-student1.study()
-under_graduate1.study()
-under_graduate1.sleep()
-student1.take_exam()
-student1.register()
-student1.sleep()
-student1.eat()
-student1.display_information()
-print(administrator1.get_role())
+student_service.register_student(student2)
+student_service.register_student(under_graduate1)
 
-administrator1.change_role("Developer", "Manager")
 
-print(administrator1.get_role())
+# Display all students
+print("\n=== ALL STUDENTS ===")
+student_service.display_all_students()
+
+
+# Find a student
+print("\n=== FIND STUDENT ===")
+student = student_service.find_student("ST001")
+
+if student:
+    student.display_information()
+else:
+    print("Student not found.")
+
+
+# Update score
+print("\n=== UPDATE SCORE ===")
+student_service.update_student_score("ST001", 85)
+
+student = student_service.find_student("ST001")
+
+if student:
+    student.display_information()
+
+
+# Student count
+print("\n=== STUDENT COUNT ===")
+print(student_service.get_student_count())
+
+
+# Average score
+print("\n=== AVERAGE SCORE ===")
+print(student_service.get_average_score())
+
+
+# Other OOP demonstration
+print("\n=== ROLES ===")
 
 people = [
     student1,
@@ -37,9 +115,5 @@ people = [
 
 for person in people:
     person.display_information()
-    print("----------------")
-print("=== ROLES ===")
-
-for person in people:
     person.perform_role()
-    
+    print("----------------")
